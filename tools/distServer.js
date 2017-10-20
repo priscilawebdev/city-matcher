43740d789a16c65e28f47cfd57675d8fadd1e9aa
Priscila Oliveira <Priscila.Oliveira@ataccama.com>
2017-10-20 15:40:13 +0200

    fixed script heroku

--- tools/distServer.js
+++ tools/distServer.js
@@ -9,12 +9,11 @@ const port = process.env.PORT || 8080
 const app = express()
 
 app.use(cors())
-app.use(express.static('dist'))
-app.use('/api', express.static(path.join(process.cwd(), 'api')))
-app.get('/', (req, res) => {
-	res.sendFile(path.join(process.cwd(), 'dist/index.html'))
-})
 
+app.use('/api', express.static(path.join(__dirname, '../api')))
+app.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname, '../dist/index.html'))
+})
 
 app.listen(port, (err) => {
 	if (err) {
